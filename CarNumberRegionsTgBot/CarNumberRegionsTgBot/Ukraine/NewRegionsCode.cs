4536d604202a5d740d73afd3df33ce9459ad102d
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2025-01-01 15:00:44 +0200

    add Ukrainian codes

---
+++ CarNumberRegionsTgBot/CarNumberRegionsTgBot/Ukraine/NewRegionsCode.cs
@@ -0,0 +1,67 @@
+﻿namespace CarNumberRegionsTgBot.Ukraine
+{
+	public class NewRegionsCode
+	{       //new 
+		public static Dictionary<string, string> UkrainianNewCarNumbersRegions = new Dictionary<string, string> //new 2004-2013
+		{
+			{ "Autonomous Republic of Crimea", "AK" },
+			{ "Vinnytsia Region", "AB" },
+			{ "Volyn Region", "AC" },
+			{ "Dnipropetrovsk Region", "AE" },
+			{ "Donetsk Region", "AH" },
+			{ "Zhytomyr Region", "AM" },
+			{ "Zakarpattia Region", "AO" },
+			{ "Zaporizhzhia Region", "AP" },
+			{ "Ivano-Frankivsk Region", "AT" },
+			{ "Kyiv", "AA" },
+			{ "Kyiv Region", "AI" },
+			{ "Kirovohrad Region", "BA" },
+			{ "Luhansk Region", "BB" },
+			{ "Lviv Region", "BC" },
+			{ "Mykolaiv Region", "BE" },
+			{ "Odesa Region", "BH" },
+			{ "Poltava Region", "BI" },
+			{ "Rivne Region", "BK" },
+			{ "Sevastopol", "CH" },
+			{ "Sumy Region", "BM" },
+			{ "Ternopil Region", "BO" },
+			{ "Kharkiv Region", "AX" },
+			{ "Kherson Region", "BT" },
+			{ "Khmelnytskyi Region", "BX" },
+			{ "Cherkasy Region", "CA" },
+			{ "Chernihiv Region", "CB" },
+			{ "Chernivtsi Region", "CE" }
+		};
+
+		public static Dictionary<string, string> UkrainianCarNumbersRegionsAfter2013 = new Dictionary<string, string> //new 2013 - present
+			{
+				{ "Autonomous Republic of Crimea", 1 },
+				{ "Vinnytsia Region", 2 },
+				{ "Volyn Region", 3 },
+				{ "Dnipropetrovsk Region", 4 },
+				{ "Donetsk Region", 5 },
+				{ "Zhytomyr Region", 6 },
+				{ "Zakarpattia Region", 7 },
+				{ "Zaporizhzhia Region", 8 },
+				{ "Ivano-Frankivsk Region", 9 },
+				{ "Kyiv", 11 },
+				{ "Kyiv Region", 10 },
+				{ "Kirovohrad Region", 12 },
+				{ "Luhansk Region", 13 },
+				{ "Lviv Region", 14 },
+				{ "Mykolaiv Region", 15 },
+				{ "Odesa Region", 16 },
+				{ "Poltava Region", 17 },
+				{ "Rivne Region", 18 },
+				{ "Sevastopol", 27 },
+				{ "Sumy Region", 19 },
+				{ "Ternopil Region", 20 },
+				{ "Kharkiv Region", 21 },
+				{ "Kherson Region", 22 },
+				{ "Khmelnytskyi Region", 23 },
+				{ "Cherkasy Region", 24 },
+				{ "Chernihiv Region", 25 },
+				{ "Chernivtsi Region", 26 }
+			};
+	}
+}
---
+++ CarNumberRegionsTgBot/CarNumberRegionsTgBot/Ukraine/OldRegionsCode.cs
@@ -0,0 +1,37 @@
+﻿namespace CarNumberRegionsTgBot.Ukraine
+{
+	public static class OldRegionsCode
+	{
+		//old
+		public static Dictionary<string, List<string>> UkrainianOldCarNumbersRegions = new Dictionary<string, List<string>>	//old 1995-2004
+		{
+			{ "Autonomous Republic of Crimea", 1 },
+			{ "Vinnytsia Region", 2 },
+			{ "Volyn Region", 3 },
+			{ "Dnipropetrovsk Region", 4 },
+			{ "Donetsk Region", 5 },
+			{ "Zhytomyr Region", 6 },
+			{ "Zakarpattia Region", 7 },
+			{ "Zaporizhzhia Region", 8 },
+			{ "Ivano-Frankivsk Region", 9 },
+			{ "Kyiv", 11 },
+			{ "Kyiv Region", 10 },
+			{ "Kirovohrad Region", 12 },
+			{ "Luhansk Region", 13 },
+			{ "Lviv Region", 14 },
+			{ "Mykolaiv Region", 15 },
+			{ "Odesa Region", 16 },
+			{ "Poltava Region", 17 },
+			{ "Rivne Region", 18 },
+			{ "Sevastopol", 27 },
+			{ "Sumy Region", 19 },
+			{ "Ternopil Region", 20 },
+			{ "Kharkiv Region", 21 },
+			{ "Kherson Region", 22 },
+			{ "Khmelnytskyi Region", 23 },
+			{ "Cherkasy Region", 24 },
+			{ "Chernihiv Region", 25 },
+			{ "Chernivtsi Region", 26 }
+		};
+	}
+}
